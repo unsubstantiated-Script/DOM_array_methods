9f472c03f025be848221d31ce554f1bf06015bdd
Justin Fulton <jusfulton@gmail.com>
2020-07-30 15:51:29 -0700

    initial setup with HTML and CSS

---
+++ script.js
@@ -0,0 +1,10 @@
+const main = document.getElementById('main')
+const addUserBtn = document.getElementById('add-user')
+const doubleBtn = document.getElementById('double')
+const showMillionaires = document.getElementById('show-millionaires')
+const sortBtn = document.getElementById('sort')
+const calculateWealthBtn = document.getElementById('calculate-wealth')
+
+let data = [];
+
+//Fetch Random User and Add Money
