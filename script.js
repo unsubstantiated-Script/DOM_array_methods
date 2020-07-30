82e7663278a6b02f8b3dd0f5225004b8f80ae3da
Justin Fulton <jusfulton@gmail.com>
2020-07-30 15:59:45 -0700

    fetching data and storing it in the arr

--- script.js
+++ script.js
@@ -6,5 +6,28 @@ const sortBtn = document.getElementById('sort')
 const calculateWealthBtn = document.getElementById('calculate-wealth')
 
 let data = [];
+getRandomUser();
+
 
 //Fetch Random User and Add Money
+async function getRandomUser() {
+    const res = await fetch('https://randomuser.me/api');
+    const data = await res.json()
+
+
+    const user = data.results[0];
+
+    const newUser = {
+        name: `${user.name.first} ${user.name.last}`,
+        money: Math.floor(Math.random() * 1000000)
+    }
+
+    addData(newUser)
+
+}
+
+//Add new object to data array
+
+function addData(obj) {
+    data.push(obj)
+}
