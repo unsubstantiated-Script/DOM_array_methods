c14367e62f6bf2041f955f125f4ace7b7fdec815
Justin Fulton <jusfulton@gmail.com>
2020-07-31 14:28:02 -0700

    project updated with js array methods

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const sortBtn = document.getElementById('sort')
 const calculateWealthBtn = document.getElementById('calculate-wealth')
 
 let data = [];
-getRandomUser();
 
 
 //Fetch Random User and Add Money
@@ -23,11 +22,77 @@ async function getRandomUser() {
     }
 
     addData(newUser)
+}
+
+//Double dat cash! 
+function doubleMoney() {
+    data = data.map(user => {
+        //Spread operator to grab old obj, manipulate one element of that object return new array of obj
+        return {
+            ...user,
+            money: user.money * 2
+        }
+    })
+
+    updateDOM();
+}
+
+//Sort users by richest
+function sortByRichest() {
+    data.sort((a, b) =>
+        b.money - a.money
+    )
+    updateDOM();
+}
+
+//Filter only millionaires
+function filterMillionaires() {
+    data = data.filter(user => user.money >= 1000000)
+    updateDOM()
+}
+
+//Add up totals
+function calculateWealth() {
+    const wealth = data.reduce((acc, user) => (acc += user.money), 0)
+
+    const wealthEl = document.createElement('div')
+    wealthEl.innerHTML = `<h3>The total amount of cash here is: <strong>$${formatMoney(wealth)}</strong></h3>`
+    main.appendChild(wealthEl)
 
 }
+
+
 
 //Add new object to data array
-
 function addData(obj) {
     data.push(obj)
+    updateDOM()
 }
+
+//Update DOM 
+
+function updateDOM(providedData = data) {
+    //Clear main DIV
+    main.innerHTML = ' <h2><strong>Person</strong> Wealth</h2>'
+
+    providedData.forEach(item => {
+        const element = document.createElement('div')
+        element.classList.add('person')
+        element.innerHTML = `<strong>${item.name}</strong> $${formatMoney(item.money)}`
+        main.appendChild(element)
+    })
+}
+
+//Format number into money
+
+function formatMoney(number) {
+    return number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+//Event Listeners
+
+addUserBtn.addEventListener('click', getRandomUser)
+doubleBtn.addEventListener('click', doubleMoney)
+showMillionaires.addEventListener('click', filterMillionaires)
+sortBtn.addEventListener('click', sortByRichest)
+calculateWealthBtn.addEventListener('click', calculateWealth)
